99789233c761735a3b64b885340777e7e0f81b21
eric <ericmullally@yahoo.com>
2019-02-05 11:17:57 -0600

    header done story in progress

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ const menuBackgrnd = document.querySelector(".menu__back");
 const list = document.querySelector(".menu__list");
 
 navBtn.addEventListener("click", () => {
+  const lines = navBtn.childNodes;
+
   if (menuBackgrnd.style.transform === "") {
     menuBackgrnd.style.transform = "scale(60)";
     list.style.right = " 50%";
-    navBtn.childNodes[1].style.transform = "rotate(45deg)";
-    navBtn.childNodes[3].style;
-    navBtn.childNodes[5].style;
+    lines[1].style.transform = "translate(50%, 10px) rotate(133deg)";
+    lines[3].style.display = "none";
+    lines[5].style.transform = "translate(50%, -12px) rotate(-131deg)";
   } else {
     menuBackgrnd.style.transform = "";
     list.style.right = "";
+    lines[1].style.transform = "";
+    lines[3].style.display = "";
+    lines[5].style.transform = "";
   }
 });
-console.log(navBtn.childNodes[1]);
-// top:50%;
-// transform: translate(50%, -50%) rotate(45%);
