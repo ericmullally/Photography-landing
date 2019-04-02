935069061a8d055c608cd78ddec5c557fdbf33a0
eric <ericmullally@yahoo.com>
2019-04-02 10:29:12 -0500

    added extra card. started implementing parallax

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 const navBtn = document.querySelector(".menu__btn");
-const menuBackgrnd = document.querySelector(".menu__back");
+const menuBackground = document.querySelector(".menu__back");
 const list = document.querySelector(".menu__list");
+
+const getScrollPos = () => {
+  return document.documentElement.scrollTop;
+};
 
 navBtn.addEventListener("click", () => {
   const lines = navBtn.childNodes;
 
-  if (menuBackgrnd.style.transform === "") {
-    menuBackgrnd.style.transform = "scale(60)";
+  if (menuBackground.style.transform === "") {
+    menuBackground.style.transform = "scale(60)";
     list.style.right = " 50%";
     lines[1].style.transform = "translate(50%, 10px) rotate(133deg)";
     lines[3].style.display = "none";
     lines[5].style.transform = "translate(50%, -12px) rotate(-131deg)";
   } else {
-    menuBackgrnd.style.transform = "";
+    menuBackground.style.transform = "";
     list.style.right = "";
     lines[1].style.transform = "";
     lines[3].style.display = "";
     lines[5].style.transform = "";
   }
+});
+const positionFromTop = this.addEventListener("scroll", e => {
+  console.log(getScrollPos());
 });
